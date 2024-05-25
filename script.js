0989fb9d0acb279131a5f57bf8fa925b309b5dfd
Izaan Khan <izaankhan.x@gmail.com>
2024-05-25 11:33:01 +0500

    added animation to cards flip js and css

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function flipper(imageElement) {
     var cardImage = "./media/card.png";
     var originalImage = imageElement.getAttribute("data-original");
-    
+    // imageElement.classList.remove('flip-2')
+    // imageElement.classList.remove('flip-1')
     if (imageElement.src.includes("card.png")) {
         imageElement.src = originalImage;
+        imageElement.classList.remove('flip-1')
+        imageElement.classList.add('flip-2')
     } else {
         imageElement.setAttribute("data-original", imageElement.src);
         imageElement.src = cardImage;
+        imageElement.classList.remove('flip-2')
+        imageElement.classList.add('flip-1')
     }
 }
